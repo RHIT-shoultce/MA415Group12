eb46b752a4f70a7d3f744a24baa3b6fa0efc7e53
RHIT-shoultce <89893056+RHIT-shoultce@users.noreply.github.com>
2025-05-16 09:14:20 -0400

    PCA added

---
+++ feature_selection.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+
+def pca(X, y, n_components=0.9):
+    # Split the dataset into training and test sets
+
+    # Perform PCA
+    print('\nApplying PCA...')
+    pca = PCA(n_components=n_components)  # You can set n_components to an integer or a percentage
+    X = pca.fit_transform(X)  # Fit and transform training data
+
+    print(f"Explained variance ratio: {pca.explained_variance_ratio_}")
+    print(f"Number of components after PCA: {X.shape[1]}")
+
+    return X
